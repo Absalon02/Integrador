b3efa645b406846a706c2425304651492be791a3
Absalon02 <ceballosabsalonpatricia@gmail.com>
2023-07-27 17:48:12 -0600

    vinculando

---
+++ src/components/Cards.jsx
@@ -0,0 +1,31 @@
+import Card from './Card';
+
+export default function Cards({characters, onClose}) {
+   return (
+   <div>
+      {characters.map(function({
+         id,   
+         name,
+         status,
+         species,
+         gender,
+         origin,
+         image
+         }){
+         return(
+            <Card
+               key={id}
+               id={id}
+               name={name}
+               status={status}
+               species={species}
+               gender={gender}
+               origin={origin.name}
+               image={image}
+               onClose={onClose}
+            />
+         )
+      })}
+   </div>
+   );
+}
